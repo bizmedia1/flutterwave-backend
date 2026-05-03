4262c93b20ca5ba8dd2cf259c7e63134b3bf3c0d
bizmedia1 <victorisrael539@gmail.com>
2026-05-03 17:38:37 +0100

    Update create-payment.js

--- api/create-payment.js
+++ api/create-payment.js
@@ -41,8 +41,8 @@ export default async function handler(req, res) {
           },
 
           customizations: {
-            title: "My Business",
-            description: "Payment",
+            title: "Glm",
+            description: "Subscription",
           },
         }),
       }
